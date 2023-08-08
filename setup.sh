88b685f300f6bc15b65946a511b1d7c3c6eb3032
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-08-08 14:58:11 +0200

    Update and rename setup_module.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# setup_module by Kurama250
+# setup.sh by Kurama250
 # Github : https://github.com/Kurama250/Alerte_DDos_L7
 
 apt update && apt upgrade -y
 apt install npm node.js zip -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
 apt-get install -y nodejs -y
+git clone https://github.com/Kurama250/Alerte_DDos_L7.git
+cd Alerte_DDos_L7/
 npm install axios tail
 npm install pm2 -g
