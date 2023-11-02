032cad6cc3c595827ad65beef76b12ae591dd762
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-11-02 02:32:24 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # setup.sh by Kurama250
-# Github : https://github.com/Kurama250/Alerte_DDos_L7
+# Github : https://github.com/Kurama250/Alert_L7_DDoS
 
 apt update && apt upgrade -y
-apt install npm node.js zip -y
+apt install npm nodejs zip -y
 curl -fsSL https://deb.nodesource.com/setup_16.x | bash - &&\
 apt-get install -y nodejs -y
-git clone https://github.com/Kurama250/Alerte_DDos_L7.git
-cd Alerte_DDos_L7/
+git clone https://github.com/Kurama250/Alert_L7_DDoS.git
+cd Alert_L7_DDoS/
 npm install axios tail
 npm install pm2 -g
