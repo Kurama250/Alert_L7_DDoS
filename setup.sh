a1c6808c025779096ec0e70e8b60fffdc99a8c21
Kurama <60360593+Kurama250@users.noreply.github.com>
2024-12-14 21:20:42 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,16 +3,23 @@
 # Github : https://github.com/Kurama250/Alert_L7_DDoS
 
 apt update && apt upgrade -y
-apt install npm nodejs zip -y
-apt update && apt upgrade -y
-apt install npm nodejs git -y
+apt install npm nodejs zip git -y
+
 if ! command -v node &> /dev/null; then
-    curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-    apt-get install -y nodejs -y
+    echo "Node.js is not installed. Installing..."
+    curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+    apt-get install -y nodejs
 else
     echo "Node.js is already installed. Skipping installation."
 fi
+
 git clone https://github.com/Kurama250/Alert_L7_DDoS.git
 cd Alert_L7_DDoS/
 npm install axios tail
-npm install pm2 -g
+
+if ! command -v pm2 &> /dev/null; then
+    echo "PM2 is not installed. Installing..."
+    npm install pm2 -g
+else
+    echo "PM2 is already installed. Skipping installation."
+fi
